d64fcc5343e7dcb7bae797ba6a663d947e1d0a99
Timothy Bostanche <tbostanche@wisc.edu>
2021-10-22 12:34:43 -0500

    Creating initial skeleton code for each activity

---
+++ app/src/main/java/com/example/roommateys/ChoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.roommateys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chore);
+    }
+}
