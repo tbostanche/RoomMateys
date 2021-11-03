9ee1e1ec7c07e71132ec9374a226c61bfea977db
andrewzagorski <andrewlzagorski@gmail.com>
2021-11-03 14:44:28 -0500

    Map Activities

---
+++ app/src/main/java/com/example/roommateys/MapActivity.java
@@ -0,0 +1,32 @@
+package com.example.roommateys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+    public void choreOnClick(View view) {
+        Intent intent = new Intent(this, ChoreActivity.class);
+        startActivity(intent);
+    }
+    public void shoppingOnClick(View view) {
+        Intent intent = new Intent(this, ShoppingActivity.class);
+        startActivity(intent);
+    }
+    public void settingsOnClick(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }
+    public void messageOnClick(View view) {
+        Intent intent = new Intent(this, MessageActivity.class);
+        startActivity(intent);
+    }
+}
