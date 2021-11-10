32681f8f59af482b4c540a987404c4e4cbe0ea17
bryce-belgarde <belgarde@wisc.edu>
2021-11-10 07:55:07 -0600

    Chore Activity functional, mimics shopping activity

--- app/src/main/java/com/example/roommateys/ChoreActivity.java
+++ app/src/main/java/com/example/roommateys/ChoreActivity.java
@@ -1,17 +1,101 @@
 package com.example.roommateys;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ChoreActivity extends AppCompatActivity {
+
+    ListView choreList;
+    ArrayList<String> choreListArray = new ArrayList<>();
+    String dialogText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chore);
+        choreList = (ListView) findViewById(R.id.choreListView);
+
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, choreListArray);
+        choreList.setAdapter(adapter);
+        choreList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Log.i("LONGCLICK", "Long click detected");
+                AlertDialog.Builder builder = new AlertDialog.Builder(ChoreActivity.this);
+                builder.setTitle("Remove Activity From Chore List");
+                builder.setMessage(String.format("Are you sure you want to remove \"%s\" from the list?", choreListArray.get(position)));
+                builder.setPositiveButton("Remove", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.i("POSITION:", "" + position);
+                        Log.i("SIZE:", "" + choreListArray.size());
+                        choreListArray.remove(position);
+                        ArrayAdapter adapter1 = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, choreListArray);
+                        choreList.setAdapter(adapter1);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                if (!isFinishing()) builder.show();
+                return false;
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.chore_list_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.newChore) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Title");
+            final EditText input = new EditText(this);
+            input.setHint("Type chore here");
+            builder.setView(input);
+            builder.setPositiveButton("Add Chore", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogText = input.getText().toString();
+                    choreListArray.add(dialogText);
+                    choreList.setAdapter(new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, choreListArray));
+
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            builder.show();
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 
     public void shoppingOnClick(View view) {
