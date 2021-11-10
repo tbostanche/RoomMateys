70dd0557cfdea2724e4fd86c88b5bfabfb597b0b
bryce-belgarde <belgarde@wisc.edu>
2021-11-09 20:41:12 -0600

    initial maps commit, map fragment operational, displays current location

--- app/src/main/java/com/example/roommateys/MapActivity.java
+++ app/src/main/java/com/example/roommateys/MapActivity.java
@@ -1,17 +1,41 @@
 package com.example.roommateys;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationRequest;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MapActivity extends AppCompatActivity {
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private LatLng currentPosition = new LatLng(0.0, 0.0);
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 12; //could've been any number!
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            displayMyLocation();
+        });
     }
     public void choreOnClick(View view) {
         Intent intent = new Intent(this, ChoreActivity.class);
@@ -29,4 +53,28 @@ public class MapActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MessageActivity.class);
         startActivity(intent);
     }
+
+    private void displayMyLocation() {
+        // check if permission is granted
+        int permission = ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION);
+        // if not, ask for it
+        if (permission == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+        // if permission is granted, display marker at current location
+        else {
+            mFusedLocationProviderClient.getCurrentLocation(LocationRequest.QUALITY_HIGH_ACCURACY, null)
+                    .addOnCompleteListener(this, task -> {
+                        Location mLastKnownLocation = task.getResult();
+                        if (task.isSuccessful() && mLastKnownLocation != null) {
+                            currentPosition = new LatLng(mLastKnownLocation.getLatitude(), mLastKnownLocation.getLongitude());
+                            mMap.addMarker(new MarkerOptions().position(currentPosition).title("My Location"));
+                        }
+                    });
+        }
+    }
+
 }
