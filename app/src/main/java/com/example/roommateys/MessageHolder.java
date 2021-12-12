8a366d00ddb74b41a28f39ab7b0e7aeb4c3d9d6e
andrewzagorski <andrewlzagorski@gmail.com>
2021-12-12 11:04:32 -0600

    Basic messaging

---
+++ app/src/main/java/com/example/roommateys/MessageHolder.java
@@ -0,0 +1,19 @@
+package com.example.roommateys;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MessageHolder extends RecyclerView.ViewHolder {
+    TextView displayName, messageText;
+    ConstraintLayout constraintLayout;
+    public MessageHolder(@NonNull View itemView) {
+        super(itemView);
+        displayName = itemView.findViewById(R.id.messageDisplayName);
+        messageText = itemView.findViewById(R.id.messageText);
+        constraintLayout = itemView.findViewById(R.id.messageConstraintLayout);
+    }
+}
